1f32f1c43ddc214e658aa57d10b97275cd365c2f
Dante Calderón <dantehemerson@gmail.com>
2018-08-05 08:58:56 -0500

    Admin site

--- polls/admin.py
+++ polls/admin.py
@@ -6,7 +6,6 @@ class QuestionAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None, 				 {'fields': ['question_text']}),
 		('Date information', {'fields': ['pub_date']}),
-	]
-	fields = ['pub_date', 'question_text']
+	]	
 
 admin.site.register(Question, QuestionAdmin)
